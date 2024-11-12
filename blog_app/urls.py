e9d979b8aec6ec0a7d0f8774a5085441cf80ffe0
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2024-11-12 18:42:14 +0530

    Model update

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.hello, name="Blog_List"),
+    path('blog_list/',views.hello, name="Blog_List"),
+    path('get_detials/<int:pk>/',views.blog_detials, name = "blog_detials"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import response, JsonResponse
+from .models import *
 
 def hello(request):
-    data = {
-        "Message": "Hi Hello Every one!!"
-        }
+    blogs = Blog.objects.all()
+
+    data ={
+        "Blogs": list(blogs.values())
+    }
     
     return JsonResponse(data)
+
+def blog_detials(request,pk):
+    blog = Blog.objects.get(pk=pk)
+
+    data={
+        "name": blog.name,
+        "description": blog.description,
+        "slug": blog.slug
+    }
+
+    return JsonResponse(data)
+
